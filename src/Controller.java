5ef54dce7524914933fb864a1e2bcd72f2c8d366
Lucas Pérez <lperezparracho@danielcastelao.org>
2023-06-19 11:45:40 +0200

    ex3 first commit

--- src/Controller.java
+++ src/Controller.java
@@ -10,23 +10,12 @@ public class Controller {
         miModelo.crearCoche("Aston Martin", "FGH 3333");
 
         Coche ferrari = miModelo.getCoche("SBC 1234");
-
         // modifica la velocidad
-        miModelo.cambiarVelocidad("SBC 1234", 20);
+        miModelo.cambiarVelocidad("SBC 1234", 30);
+
         // recoje la velocidad y la muestra (tarea de la View)
         boolean hecho = miVista.muestraVelocidad("SBC 1234", miModelo.getVelocidad("SBC 1234"));
-        System.out.println(hecho);
 
-        // sube la velocidad
-        miModelo.subirVelocidad("SBC 1234", 200);
-        // recoje la velocidad y la muestra (tarea de la View)
-        hecho = miVista.muestraVelocidad("SBC 1234", miModelo.getVelocidad("SBC 1234"));
-        System.out.println(hecho);
-
-        // baja la velocidad
-        miModelo.bajarVelocidad("SBC 1234", 80);
-        // recoje la velocidad y la muestr (tarea de la View)
-        hecho = miVista.muestraVelocidad("SBC 1234", miModelo.getVelocidad("SBC 1234"));
         System.out.println(hecho);
     }
 }
